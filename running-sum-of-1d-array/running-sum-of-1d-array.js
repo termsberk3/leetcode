af8d08c1d1fb4aa87ad3675fe494b654aca1f7ea
Berk Çavuş <25158444+termsberk3@users.noreply.github.com>
2021-06-19 01:19:27 +0300

    Time: 72 ms (96.09%), Space: 40 MB (10.51%) - LeetHub

---
+++ running-sum-of-1d-array/running-sum-of-1d-array.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+ const numbers = [];
+var runningSum = function(nums) {
+    for(var i=1;i<nums.length;i++){
+              nums[i] = nums[i] + nums[i - 1];
+    }
+    return nums
+};
